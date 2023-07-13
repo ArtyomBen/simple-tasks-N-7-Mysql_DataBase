11c66eea8ac41d901556174331c71b019979f887
Artyom <132067879+TyomBen@users.noreply.github.com>
2023-07-13 16:10:51 +0400

    little changes

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $conn = mysqli_connect ($servername, $username, $password, $dbname);
 if (!$conn) {
     die ("Connection failed: " . mysqli_connect_error ());
 }
+
 // if (!$conn){
 //     die ("connection failes: " . mysqli_connect_error ()) ;
 // }
@@ -19,7 +20,7 @@ if (!$conn) {
 
 // 1.
 
-// $sql = "SELECT * FROM for_fun WHERE email = 'john@example.com'";
+// $sql = "SELECT firstname, lastname FROM for_fun WHERE email = 'john@example.com'";
 
 // $result = mysqli_query ($conn, $sql);
 // if (mysqli_num_rows($result ) > 0) {
@@ -39,16 +40,26 @@ if (!$conn) {
 
 //2.
 
-// $sql = "DELETE FROM for_fun WHERE email = 'john@example.com'";
-// if (mysqli_query($conn, $sql)) {
-//     echo 'Deleted';
+// $sql = "SELECT id FROM for_fun WHERE email = 'john@example.com'";
+// $result = mysqli_query ($conn, $sql);
+// if (mysqli_num_rows ($result) > 0){
+//     while ($row = mysqli_fetch_assoc ($result)){
+//         $id = $row['id'];
+//        $sql_Delete = "DELETE FROM for_fun WHERE id = $id";
+//        if (mysqli_query($conn, $sql_Delete)){
+//         echo 'Deleted';
+//        }
+//     }
+    
+// }else {
+//     echo "0 results";
 // }
 //    mysqli_close ($conn);
 
 
 //3.
 
-// $sql = "SELECT * FROM country ORDER BY Population DESC LIMIT 0, 10";
+// $sql = "SELECT Name, Population FROM country ORDER BY Population DESC LIMIT 0, 10";
 // $result = mysqli_query ($conn, $sql);
 
 // if (mysqli_num_rows ($result) > 0){
@@ -62,7 +73,7 @@ if (!$conn) {
 
 //3.1
 
-// $sql = "SELECT * FROM country ORDER BY SurfaceArea ASC LIMIT 0, 10";
+// $sql = "SELECT Name, SurfaceArea FROM country ORDER BY SurfaceArea ASC LIMIT 0, 10";
 // $result = mysqli_query ($conn, $sql);
 
 // if (mysqli_num_rows($result) > 0){
@@ -76,7 +87,7 @@ if (!$conn) {
 
 //3.2
 
-// $sql = "SELECT * FROM country ORDER BY Name ASC LIMIT 100, 100";
+// $sql = "SELECT Name FROM country ORDER BY Name ASC LIMIT 100, 100";
 // $result = mysqli_query ($conn, $sql);
 
 // if (mysqli_num_rows($result) > 0){
@@ -90,7 +101,7 @@ if (!$conn) {
 
 //3.3
 
-// $sql = "SELECT * FROM country WHERE IndepYear IS NOT NULL ORDER BY IndepYear ASC LIMIT 0, 1";
+// $sql = "SELECT Name, IndepYear FROM country WHERE IndepYear IS NOT NULL ORDER BY IndepYear ASC LIMIT 0, 1";
 // $result = mysqli_query ($conn, $sql);
 
 // if (mysqli_num_rows ($result) > 0){
@@ -100,4 +111,5 @@ if (!$conn) {
 // }else {
 //     echo "0 results";
 // }
+
 ?>
